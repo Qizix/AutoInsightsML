fe23746670bc6390bab88e9c4872ee0c11c7fb5a
Yaroslav Dziubenko <Yar.dziub2005@gmail.com>
2025-06-21 16:37:49 +0300

    feat(services): add test for reader

--- app/services/reader.py
+++ app/services/reader.py
@@ -3,12 +3,9 @@ import pandas as pd
 class DataLoader:
     def __init__(self, file_path: str):
         self.file_path = file_path
-        self.df = None
+        self.df = pd.read_csv(self.file_path)
         self.column_types = {}
         
-    def load(self):
-        self.df = pd.read_csv(self.file_path)
-    
     def define_columns(self):
         self.column_types['numerical'] = self.df.select_dtypes(include='number').columns.tolist()
         self.column_types['categorical'] = self.df.select_dtypes(include='object').columns.tolist()
@@ -16,3 +13,8 @@ class DataLoader:
     
     def check_nulls(self):
         return self.df.isnull().sum()
+    
+if __name__ == '__main__':
+    dl = DataLoader('iris.csv')
+    print(dl.define_columns())
+    print(dl.check_nulls())
