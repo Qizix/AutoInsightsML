98e657eccdc46fdc42252be9e1f24a037841b76a
qzx <yar.dziub2005@gmail.com>
2025-06-29 17:51:58 +0300

    fix: refactor comments

--- app.py
+++ app.py
@@ -3,24 +3,22 @@ import streamlit as st
 import pandas as pd
 from services import DataCleaner, DataLoader
 
-# Load menu
-file = None
 st.title("Auto Insights")
 
-# Frame with data
+# load data, data loaded in object DataLoader
 if "dl" not in st.session_state:
     file = st.file_uploader("Upload your CSV", type="csv")
     dl = DataLoader(file)
     st.session_state["dl"] = dl
     st.rerun()
 
-dataframe = st.session_state["dl"].dataframe()
-st.write(st.session_state["dl"].check_nulls())
-# st.write(dl.define_columns())
-st.data_editor(dataframe)
+# showing table
+st.write(st.session_state["dl"].check_nulls()) # need to show as additional columns under the table
+st.data_editor(st.session_state["dl"].dataframe())
 
 # Action buttons
 fill_missing_values_button, _, _ = st.columns(3)
+# Fill missing data, using DataCleaner
 if fill_missing_values_button.button("Fill missing values", use_container_width=True):
     dc = DataCleaner(st.session_state["dl"].dataframe(), st.session_state["dl"].define_columns())
     st.session_state["dl"].df = dc.handle_missing()
