05029615c885dae464d4308e8691f9ed99b65cbb
qzx <yar.dziub2005@gmail.com>
2025-06-30 15:40:46 +0300

    feat: save table

--- app.py
+++ app.py
@@ -16,9 +16,7 @@ else:
     st.dataframe(st.session_state["dl"].check_nulls().to_frame().T)
     st.data_editor(st.session_state["dl"].dataframe())
 
-    fill_missing_values, qfill_missing_values_button, wfill_missing_values_button = (
-        st.columns(3)
-    )
+    fill_missing_values, qfill_missing_values_button, save_csv = st.columns(3)
     # Fill missing data, using DataCleaner
     with fill_missing_values:
         is_missing = bool(st.session_state["dl"].check_nulls().sum() == 0)
@@ -29,17 +27,22 @@ else:
         fill_missing_values_selectors_container = st.container(border=True)
         with fill_missing_values_selectors_container:
             num_strat = st.segmented_control(
-                "Numerical filling strategy", ["mean", "median"], default="mean",
-                disabled=is_missing
+                "Numerical filling strategy",
+                ["mean", "median"],
+                default="mean",
+                disabled=is_missing,
             )
             col_strat = st.segmented_control(
-                "Categorical filling strategy", ["Unknown", "mode"], default="Unknown",
-                disabled=is_missing
+                "Categorical filling strategy",
+                ["Unknown", "mode"],
+                default="Unknown",
+                disabled=is_missing,
             )
 
         if fill_missing_values_button:
             dc = DataCleaner(
-                st.session_state["dl"].dataframe(), st.session_state["dl"].define_columns()
+                st.session_state["dl"].dataframe(),
+                st.session_state["dl"].define_columns(),
             )
             st.session_state["dl"].df = dc.handle_missing(num_strat, col_strat)
             st.rerun()
@@ -47,17 +50,17 @@ else:
     with qfill_missing_values_button:
         if qfill_missing_values_button.button("pass1", use_container_width=True):
             dc = DataCleaner(
-                st.session_state["dl"].dataframe(), st.session_state["dl"].define_columns()
+                st.session_state["dl"].dataframe(),
+                st.session_state["dl"].define_columns(),
             )
             st.session_state["dl"].df = dc.handle_missing()
             st.rerun()
         st.segmented_control("pass1", ["meanasdf", "mediasdfn"])
 
-    with wfill_missing_values_button:
-        if wfill_missing_values_button.button("pass2", use_container_width=True):
-            dc = DataCleaner(
-                st.session_state["dl"].dataframe(), st.session_state["dl"].define_columns()
-            )
-            st.session_state["dl"].df = dc.handle_missing()
-            st.rerun()
-        st.segmented_control("pass2", ["123mean", "234median"])
+    with save_csv:
+        st.download_button(
+            "Save table in csv format", use_container_width=True,
+            data = st.session_state["dl"].save_csv(),
+            file_name="table.csv", # need fix to loaded name
+            mime="text/csv"
+        )
