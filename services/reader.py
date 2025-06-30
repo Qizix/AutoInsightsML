7b45b84f64c00e95ee4141c8ac5ce61058120756
qzx <yar.dziub2005@gmail.com>
2025-06-30 15:33:25 +0300

    feat: added method save_csv, and formatted code m

--- services/reader.py
+++ services/reader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class DataLoader:
     def __init__(self, file_path: str):
         self.file_path = file_path
@@ -10,16 +11,24 @@ class DataLoader:
         return self.df
 
     def define_columns(self):
-        self.column_types['numerical'] = self.df.select_dtypes(include='number').columns.tolist()
-        self.column_types['categorical'] = self.df.select_dtypes(include='object').columns.tolist()
+        self.column_types["numerical"] = self.df.select_dtypes(
+            include="number"
+        ).columns.tolist()
+        self.column_types["categorical"] = self.df.select_dtypes(
+            include="object"
+        ).columns.tolist()
         return self.column_types
 
     def check_nulls(self):
         return self.df.isnull().sum()
 
-if __name__ == '__main__':
-    dl = DataLoader('iris.csv')
+    def save_csv(self):
+        return self.df.to_csv(index=False).encode("utf-8")
+
+
+if __name__ == "__main__":
+    dl = DataLoader("iris.csv")
     print(dl.define_columns())
     print(dl.check_nulls())
-    if dl.check_nulls().sum():
+    if 0:
         print("zxc")
