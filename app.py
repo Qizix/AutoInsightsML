2074e99dffe80880495d60e36abe42454ccc41ab
qzx <yar.dziub2005@gmail.com>
2025-07-01 15:34:42 +0300

    feat(missing data): drop missing values connected to ui + code formating

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ else:
     st.dataframe(st.session_state["dl"].check_nulls().to_frame().T)
     st.data_editor(st.session_state["dl"].dataframe())
 
-    fill_missing_values, qfill_missing_values_button, save_csv = st.columns(3)
+    fill_missing_values, drop_missing_values, save_csv = st.columns(3)
     # Fill missing data, using DataCleaner
     with fill_missing_values:
         is_missing = bool(st.session_state["dl"].check_nulls().sum() == 0)
@@ -47,15 +47,34 @@ else:
             st.session_state["dl"].df = dc.handle_missing(num_strat, col_strat)
             st.rerun()
 
-    with qfill_missing_values_button:
-        if qfill_missing_values_button.button("pass1", use_container_width=True):
-            dc = DataCleaner(
-                st.session_state["dl"].dataframe(),
-                st.session_state["dl"].define_columns(),
+    with drop_missing_values:
+        drop_missing_values_button = st.button(
+            "Drop missing values", use_container_width=True, disabled=is_missing
+        )
+        drop_missing_values_selectors_container = st.container(border=True)
+        with drop_missing_values_selectors_container:
+            option_map = {0: "Rows", 1: "Cols"}
+            drop_type = st.segmented_control(
+                "Select drop type",
+                option_map.keys(),
+                disabled=is_missing,
+                format_func=lambda option: option_map[option],
+                default=0,
             )
-            st.session_state["dl"].df = dc.handle_missing()
-            st.rerun()
-        st.segmented_control("pass1", ["meanasdf", "mediasdfn"])
+            drop_rate = st.select_slider(
+                "How much % missing data needed to drop:", options=range(101),
+                disabled=is_missing,
+            )
+            if drop_missing_values_button:
+                dc = DataCleaner(
+                    st.session_state["dl"].dataframe(),
+                    st.session_state["dl"].define_columns(),
+                )
+                st.session_state["dl"].df = dc.drop_with_missing_data(
+                    drop_rate / 100,
+                    drop_type,
+                )
+                st.rerun()
 
     with save_csv:
         st.download_button(
